cfe1954644cb701a12cb93d9435ac06420e0279e
Wylhelm <30578947+Wylhelm@users.noreply.github.com>
2024-11-10 19:50:44 -0500

    Update config

--- config.py
+++ config.py
@@ -101,7 +101,7 @@ class Config:
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S%f'
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger('config')
 
